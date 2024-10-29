1079dd065e53f7059ebc5d6ecb59b6fc147c0f19
natnael <natnaelmessay477@gmail.com>
2024-10-29 11:21:27 +0300

    cors issue

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors=require("cors")
+app.use(cors())
 
 class EthiopianDate {
     constructor(year, month, day, hours, minutes, seconds) {
